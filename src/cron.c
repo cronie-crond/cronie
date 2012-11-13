c58b1d9cf6ac819186d3c7e8618a716fe25b40ec
Marcela Mašláňová <mmaslano@redhat.com>
2012-11-13 08:07:56 +0100

    Documentation improvements based on Michael Lass suggestions.

--- src/cron.c
+++ src/cron.c
@@ -155,6 +155,7 @@ static void usage(void) {
 		[-m <mail command>] off or specify prefered client for sending mails \n \
 		[-n] run in foreground \n \
 		[-p] permit any crontab \n \
+		[-P] change PATH \n \
 		[-c] enable clustering support \n \
 		[-s] log into syslog instead of sending mails \n \
 		[-x [",
