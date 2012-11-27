cronie-crond/cronie

commit c58b1d9cf6ac819186d3c7e8618a716fe25b40ec

Documentation improvements based on Michael Lass suggestions.

man/cron.8
@@ -27,7 +27,7 @@
 crond \- daemon to execute scheduled commands
 .SH SYNOPSIS
 .B crond
-.RB [ -n " | " -p " | " -s " | " -c " | " -m \fP\fI<mail command>\fP ]
+.RB [ -c " | " -h " | " -i " | " -n " | " -p " | " -P " | " -s " | " -m \fP\fI<mail command>\fP ]
 
 .B crond
 .B -x 
@@ -133,6 +133,11 @@ The daemon loads the PAM environment from the pam_env module. This
 can be overridden by defining specific settings in the appropriate crontab file.
 .SH "OPTIONS"
 .TP
+.B "\-h"
+Prints a help message and exits.
+.TP
+.B "\-i"
+Disables inotify support.
 .B "\-m"
 This option allows you to specify a shell command to use for sending 
 .I Cron 
@@ -152,6 +157,9 @@ Tells the daemon to run in the foreground. This can be useful when starting it o
 Allows 
 .I Cron
 to accept any user set crontables.
+.TP
+.B "\-P"
+Change PATH.
 .TP
 .B "\-c"
 This option enables clustering support, as described below.

src/cron.c
@@ -155,6 +155,7 @@ static void usage(void) {
 		[-m <mail command>] off or specify prefered client for sending mails \n \
 		[-n] run in foreground \n \
 		[-p] permit any crontab \n \
+		[-P] change PATH \n \
 		[-c] enable clustering support \n \
 		[-s] log into syslog instead of sending mails \n \
 		[-x [",

commit c0c663de596eeb512b23c8bd703d43e649baa9ba

Fix some issues introduced in c58b1d9 Documentation improvements based on Michael Lass suggestions.
* give -m an own line in manpage
* don't explain -P twice
* correct description of -P to what it actually does

Signed-off-by: Marcela Mašláňová <mmaslano@redhat.com>

man/cron.8
@@ -141,6 +141,7 @@ Prints a help message and exits.
 .TP
 .B "\-i"
 Disables inotify support.
+.TP
 .B "\-m"
 This option allows you to specify a shell command to use for sending
 .I Cron
@@ -163,7 +164,7 @@ Allows
 to accept any user set crontables.
 .TP
 .B "\-P"
-Change PATH.
+Don't set PATH.  PATH is instead inherited from the environment.
 .TP
 .B "\-c"
 This option enables clustering support, as described below.
@@ -179,9 +180,6 @@ installed or if mail is disabled.
 .TP
 .B "\-x"
 This option allows you to set debug flags.
-.TP
-.B "\-P"
-Don't set PATH.  PATH is instead inherited from the environment.
 .SH SIGNALS
 When the
 .I SIGHUP

src/cron.c
@@ -179,7 +179,7 @@ static void usage(void) {
 		[-m <mail command>] off or specify prefered client for sending mails \n \
 		[-n] run in foreground \n \
 		[-p] permit any crontab \n \
-		[-P] change PATH \n \
+		[-P] don't set PATH \n \
 		[-c] enable clustering support \n \
 		[-s] log into syslog instead of sending mails \n \
 		[-x [",