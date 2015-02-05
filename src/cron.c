05f4aaf8a456a4af7a4f6361600c5c7f745bd3fd
Tomas Mraz <tmraz@fedoraproject.org>
2015-02-05 16:29:49 +0100

    Do not overwrite the last zero-byte.

--- src/cron.c
+++ src/cron.c
@@ -261,7 +261,7 @@ int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "");	/* set locale to system defaults or to
 							 * that specified by any  LC_* env vars */
 	if ((cs = nl_langinfo(CODESET)) != 0L)
-		strncpy(cron_default_mail_charset, cs, MAX_ENVSTR);
+		strncpy(cron_default_mail_charset, cs, MAX_ENVSTR-1);
 	else
 		strcpy(cron_default_mail_charset, "US-ASCII");
 
