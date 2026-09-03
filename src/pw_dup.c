3ba16dc9bc27b428437252da59060d5ead6025ce
panzhe0328 <panzhe@kylinos.cn>
2026-09-03 17:32:46 +0800

    pw_dup: Fix compilation with LOGIN_CAP
    Add the missing declaration of csize so that pw_dup()
    compiles on systems with LOGIN_CAP enabled.

--- src/pw_dup.c
+++ src/pw_dup.c
@@ -48,6 +48,9 @@ struct passwd *
 pw_dup(const struct passwd *pw) {
 	char		*cp;
 	size_t		 nsize=0, psize=0, gsize=0, dsize=0, ssize=0, total;
+#ifdef LOGIN_CAP
+        size_t           csize=0;
+#endif
 	struct passwd	*newpw;
 
 	/* Allocate in one big chunk for easy freeing */
